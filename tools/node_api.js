fb1eb763ae52a9655cae33c891d4041ad2f318ec
Andrey Galkin <andrey@futoin.org>
2019-11-02 20:50:47 +0200

    CHANGED: to provide more convenient Node.js API

---
+++ tools/node_api.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * @file
+ *
+ * Copyright 2014-2019 FutoIn Project (https://futoin.org)
+ * Copyright 2014-2019 Andrey Galkin <andrey@futoin.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+exports.DRAFT_SPEC_DIR = `${__dirname}/../draft/meta`;
+exports.FINAL_SPEC_DIR = `${__dirname}/../final/meta`;
+
+exports.SPEC_DIRS = [
+    exports.DRAFT_SPEC_DIR,
+    exports.FINAL_SPEC_DIR,
+];
+
+Object.freeze(exports.SPEC_DIRS);
+Object.freeze(exports);
